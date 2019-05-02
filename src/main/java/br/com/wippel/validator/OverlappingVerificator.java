5e9f1945de09ccd34653bac5e9badcc1ce5253e0
mawippel <mawippel2@hotmail.com>
2019-05-02 09:11:54 -0300

    feat: private constructor

--- src/main/java/br/com/wippel/validator/OverlappingVerificator.java
+++ src/main/java/br/com/wippel/validator/OverlappingVerificator.java
@@ -4,19 +4,27 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 /**
- * Operations on {@link LocalDate} and {@link LocalDateTime} to
- * verify date/time overlapping.
+ * Operations on {@link LocalDate} and {@link LocalDateTime} to verify date/time
+ * overlapping.
  * 
  * @author Marcelo Wippel
  */
-public class OverlappingVerificator {
-	
-	public static boolean isOverlap(LocalDate comparableInit, LocalDate comparableEnd, LocalDate toCompareInit, LocalDate toCompareEnd) {
+public final class OverlappingVerificator {
+
+	/**
+	 * Private default constructor to avoid instantiation of this class
+	 */
+	private OverlappingVerificator() {
+	}
+
+	public static boolean isOverlap(LocalDate comparableInit, LocalDate comparableEnd, LocalDate toCompareInit,
+			LocalDate toCompareEnd) {
 		return false;
 	}
-	
-	public static boolean isOverlap(LocalDateTime comparableInit, LocalDateTime comparableEnd, LocalDateTime toCompareInit, LocalDateTime toCompareEnd) {
+
+	public static boolean isOverlap(LocalDateTime comparableInit, LocalDateTime comparableEnd,
+			LocalDateTime toCompareInit, LocalDateTime toCompareEnd) {
 		return false;
 	}
-	
+
 }
